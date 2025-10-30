3961b9520eed6348312f95b879d2995676549044
vikas <vikasbind960@gmail.com>
2025-10-30 21:47:23 +0530

    add comment

--- imageResized.py
+++ imageResized.py
@@ -1,7 +1,8 @@
 import cv2
 image = cv2.imread("1340473.png")
 if image is not None:
-    resized = cv2.resize(image, (300,200)) # to resize the image
+    resized = cv2.resize(image, (300,200)) # to resize the image (width , height)
+    #first with then height
 
 
     cv2.imshow("image resize tab ", resized)
