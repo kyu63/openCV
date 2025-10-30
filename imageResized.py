8dd9fb9b1d4b7a7885ab7809e12116957ece2fc0
vikas <vikasbind960@gmail.com>
2025-10-30 21:39:32 +0530

    first commit

---
+++ imageResized.py
@@ -0,0 +1,10 @@
+import cv2
+image = cv2.imread("1340473.png")
+if image is not None:
+    resized = cv2.resize(image, (300,200)) # to resize the image
+    cv2.imshow("image resize tab ", resized)
+    cv2.imshow("image tab", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("image not loaded")
