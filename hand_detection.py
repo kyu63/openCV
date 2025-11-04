d27a8f99b94ab1bfd937069e104da722b5886fc7
vikas <vikasbind960@gmail.com>
2025-11-05 01:25:46 +0530

    first commit

---
+++ hand_detection.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(
+    max_num_hands=3,         # detect up to 2 hands
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+) as hands:
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.flip(frame, 1)  # mirror image
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb_frame)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
+                )
+
+        cv2.imshow("Hand Detection", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
