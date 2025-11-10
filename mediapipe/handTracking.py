677dffff34b0bc9a3dee2751a8420faaf695bd58
vikas <vikasbind960@gmail.com>
2025-11-10 23:52:19 +0530

    add circle

--- mediapipe/handTracking.py
+++ mediapipe/handTracking.py
@@ -22,16 +22,23 @@ with mpHands.Hands(
         results = hands.process(imgRGB)
         if results.multi_hand_landmarks:
             for handlms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handlms.landmark):
+                    h, w, c = img.shape
+                    cx , cy = int(lm.x * w) , int(lm.y * h)
+                    if id == 12:
+                        cv2.circle(img,(cx,cy), 10, (255,0,255), cv2.FILLED)
+
                 mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
+
 
         horizental = cv2.flip(img, 1)
 
         ctime = time.time()
         fps = 1/(ctime - ptime)
         ptime = ctime
-        cv2.putText(horizental, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 2 , (255,0,255), 3)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 2 , (255,0,255), 3)
 
-        cv2.imshow("camera", horizental)
+        cv2.imshow("camera", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
